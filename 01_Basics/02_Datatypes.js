3ed65351239931175e2305b545434097a908fcad
Anmol Kumar <105695160+Anmol8611@users.noreply.github.com>
2023-01-04 14:49:51 +0000

    learned about 02_Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // it means all JS code as newer version
+
+// alert(3+3) // this is the syntax of browsers, it won't work directly on nodeJS
+
+// console.log(3+3); console.log("Anmol"); // It will work but it is not readable so do not write this way, maintain readability
+
+
+let name = "Anmol" // string datatype
+let age = 20 // integer/number datatype
+let isLogged = false // Boolean datatype
+
+// number => 2 to power 53
+// bigint // used for very big values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+// Objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // it is an Object
