600e5106347ff44d422fcfc4e8e663d1a849a2b0
Anmol Kumar <105695160+Anmol8611@users.noreply.github.com>
2023-01-04 14:31:29 +0000

    // learned variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+const accountID = 144553
+let accountEmail = "anmol@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+
+// accountID = 23  // Constants can't be changed, so we commented it.
+accountEmail = "kumar@gmail.com"
+accountPassword = "32323"
+accountCity = "Patna"
+
+console.table([accountID,accountEmail, accountPassword, accountCity]);
