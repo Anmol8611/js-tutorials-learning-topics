051d9e3becf8d96fa06056d6e25aea8defb3f450
Anmol Kumar <105695160+Anmol8611@users.noreply.github.com>
2023-03-01 06:50:33 +0000

    abhi kuch nahi sikhe hai

---
+++ 01_Basics/memorySummary.js
@@ -0,0 +1,8 @@
+// here we will learn about Stack and Heap memory
+// Stack (Primitve), Heap (Non-Primitive).
+// i.e. Generally Primitive data types are stored in Stack memory and Non-Primitive datatypes are stored in Heap memory.
+
+let myYoutubeName = "hiteshchoudharydotcom"
+
+let anotherName = myYoutubeName
+console.log();
