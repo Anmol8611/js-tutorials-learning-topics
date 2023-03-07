50ffc7ab2f7dd333a2f4264583eafb67cf098ff5
“Anmol8611” <kumaranmol8611@gmail.com>
2023-03-07 13:01:37 +0530

    memmoryconcepts are learnt

--- 01_Basics/memorySummary.js
+++ 01_Basics/memorySummary.js
@@ -7,6 +7,16 @@ let myYoutubeName = "hiteshchoudharydotcom"
 let anotherName = myYoutubeName
 anotherName = "chaiaurcode";
 console.log(anotherName);
-console.log(myYoutubeName);
+console.log(myYoutubeName); // here the older value is not changed rather the copied value is changed
 
-let user = "anmol kumar";
+
+//lets check it on object datatypes
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "anmol@google.com"
+console.log(userOne.email);
+console.log(userTwo.email); // here you will see the value has changed
